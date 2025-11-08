5ab445e03cd917d70c295d71b01e03bd0533cf54
delowardhushor <33928846+delowardhushor@users.noreply.github.com>
2025-11-08 18:54:38 +0600

    fixed env issue

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,9 @@
 import app from "./app";
+import dotenv from "dotenv";
+dotenv.config();
 import { connectDB } from "./config/db";
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5555;
 
 connectDB();
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
